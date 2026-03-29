c6eb4bb5f78aeef8f37d4a23249e8ad422f52c8f
anonimo3967-alt <anonimo3967@gmail.com>
2026-03-28 21:46:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,7 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 st.title("Reconocimiento de Imágenes")
 #st.write("Versión de Python:", platform.python_version())
 
-col1, col2 = st.columns(2)
 
-with col1:
-    st.header('J´suis canon')
-    st.write('Para obtener esta respuesta del modelo, debes hacer un gesto más o menos así:')
-
-
-with col2:
-    st.header('Bon')
-    st.write('Para obtener esta respuesta del modelo, el gesto ahora tendría que ser así:')
 
  
 
