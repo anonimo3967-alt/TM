81179f8f522126b8a5504c2de7dc9804e8813001
anonimo3967-alt <anonimo3967@gmail.com>
2026-03-28 21:38:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Reconocimiento de Imágenes")
 #st.write("Versión de Python:", platform.python_version())
+
 col1, col2 = st.columns(2)
 
 with col1:
     st.header('J´suis canon')
     st.write('Para obtener esta respuesta del modelo, debes hacer un gesto más o menos así:')
-image = Image.open('canon')
-st.image(image, width=350)
+    image = Image.open('canon')
+    st.image(image, width=350)
 
 with col2:
     st.header('Bon')
     st.write('Para obtener esta respuesta del modelo, el gesto ahora tendría que ser así:')
-
-image2 = Image.open('bon')
-st.image(image2, width=350)
+    image2 = Image.open('bon')
+    st.image(image2, width=350)
 
 with st.sidebar:
     st.subheader("Usando un modelo entrenado en teachable Machine puedes Usarlo en esta app para identificar gestos y cosas así")
